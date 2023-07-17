20d92d67fe806844a87235d9d40e131b27be49d9
Paul Levites <Paullevites84@gmail.com>
2023-07-16 18:05:22 -0700

    fixes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - function
+ *
+ * Return: Always succcess(0)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
+
+	return (0);
+}
