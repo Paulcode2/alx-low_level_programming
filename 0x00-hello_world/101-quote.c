0b20032a4287b3ccfbf87f1c01516898f3fd27c2
Paul Levites <Paullevites84@gmail.com>
2023-07-16 18:22:25 -0700

    fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,9 @@
  *
  * Return: Always Success(1)
  */
-/* betty style doc for function main goes there */
+
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
-
-
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
