55a93d219032bd2457c3ba478038f2e57b7d6775
Paul Levites <Paullevites84@gmail.com>
2023-07-16 18:19:13 -0700

    fixes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/* more headers goes there */
+/**
+ * main - function
+ *
+ * Return: Always Success(1)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+
+
+	return (1);
+}
