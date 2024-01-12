cadbf3f7a09415e12e4dc1a8aa0348e45344dbf3
paul levites <paullevites84@gmail.com>
2024-01-12 15:35:13 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints name
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
